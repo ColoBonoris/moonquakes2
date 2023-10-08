56700dfe1fe5317a9c00dd989df0a3662176b208
Matias Gonzalez <MatiasG2001@outlook.com>
2023-10-08 00:54:55 -0300

    envio modificacion del deep quake

--- src/lib/three/quakes/quake.ts
+++ src/lib/three/quakes/quake.ts
@@ -1,6 +1,6 @@
 import { RAYCASTER_CHANNEL, UNIT_TO_KM } from '$lib/three/constants';
 import { MOON_UNIT_RADIUS } from '$lib/three/moon';
-import { BoxGeometry, CapsuleGeometry, CylinderGeometry, Mesh, MeshBasicMaterial, SphereGeometry } from 'three';
+import { BoxGeometry, CapsuleGeometry, CylinderGeometry, Mesh, MeshBasicMaterial, SphereGeometry, Vector3 } from 'three';
 import type { QuakeData, QuakeType } from './types';
 import { Pulse } from './wave/Pulse';
 
@@ -51,26 +51,32 @@ function createLabel(data: QuakeData) {
 
 export function createMesh(radiusToOrigin: number, lat: number, lon: number, depth: number, type: QuakeType) {
   const profundity = depth * UNIT_TO_KM;
-  let geo = undefined
-  console.log(type);
+  let geo = undefined;
+  let newQuake = null;
+  const phi = (90 - lon) * (Math.PI / 180);
+  const theta = (lat + 180) * (Math.PI / 180);
   if(type.match(/A\d+/)) {
-    geo = depth > 0.1 ? new BoxGeometry(SIZE, SIZE, profundity) : geometry;
+    geo = new BoxGeometry(SIZE, SIZE, profundity);
+    newQuake = new Mesh(geo, getMesh(type));
+    newQuake.position.set(
+      -((radiusToOrigin - (profundity / 2)) * Math.sin(phi) * Math.cos(theta)),
+      -((radiusToOrigin - (profundity / 2)) * Math.sin(phi) * Math.sin(theta)),
+      -((radiusToOrigin - (profundity / 2)) * Math.cos(phi)),
+    );
   }
   else{
     geo = new SphereGeometry(5 * UNIT_TO_KM, 64, 32);
+    newQuake = new Mesh(geo, getMesh(type));
+    newQuake.position.set(
+      -((radiusToOrigin - profundity / 2) * Math.sin(phi) * Math.cos(theta)),
+      -((radiusToOrigin - profundity / 2) * Math.sin(phi) * Math.sin(theta)),
+      -((radiusToOrigin - profundity / 2) * Math.cos(phi)),
+    );
   }
-
-  const newQuake = new Mesh(geo, getMesh(type));
-  const phi = (90 - lon) * (Math.PI / 180), theta = (lat + 180) * (Math.PI / 180);
-  newQuake.position.set(
-    -((radiusToOrigin - profundity / 2) * Math.sin(phi) * Math.cos(theta)),
-    -((radiusToOrigin - profundity / 2) * Math.sin(phi) * Math.sin(theta)),
-    -((radiusToOrigin - profundity / 2) * Math.cos(phi)),
-  );
   newQuake.visible = false;
   newQuake.name = 'quake';
   newQuake.layers.enable(RAYCASTER_CHANNEL);
-  newQuake.lookAt(0, 0, -1);
+  newQuake.lookAt(0, 0, 0);
   return newQuake;
 }
 
@@ -94,8 +100,22 @@ export class Quake {
       this.pulse = new Pulse(SIZE * 7, color);
     }
 
-    
-    this.pulse.position.copy(this.mesh.position);
+    if(data.type.match(/A\d+/)){
+      const phi = (90 - data.longitude) * (Math.PI / 180), theta = (data.latitude + 180) * (Math.PI / 180);
+      let position = new Vector3(this.mesh.position.x, this.mesh.position.y, this.mesh.position.z)
+      position.set(
+        -((DISTANCE_TO_WORLD_ORIGIN ) * Math.sin(phi) * Math.cos(theta)),
+        -((DISTANCE_TO_WORLD_ORIGIN ) * Math.sin(phi) * Math.sin(theta)),
+        -((DISTANCE_TO_WORLD_ORIGIN ) * Math.cos(phi)),
+      );
+
+      console.log(position);
+      this.pulse.position.copy(position);
+    }
+    else {
+      this.pulse.position.copy(this.mesh.position);
+    }
+
     this.pulse.lookAt(0, 0, 0);
     this.label = createLabel(data);
     this.depth = data.depth;
